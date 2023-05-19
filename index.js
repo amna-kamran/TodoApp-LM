563a5b790741979292b8d5ffa6b5447f29be0a11
jurazil <jurazilgreen@gmail.com>
2023-05-20 04:02:42 +0500

    checked/unchecked toggle + delete task

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ function addTask() {
   }
   inputBox.value = "";
 }
+
+//coding the function where if we click on the task, it gets toggled between being checked
+//and unchecked
+
+//we do this by first adding the event listener to the list-container which is the id name
+//of the ul tag inside which we keep all the tasks
+function del(e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+  }
+}
+listContainer.addEventListener("click", del, false);
