daa19d143fb7541495e1fd7c165b027e104644ea
jurazil <jurazilgreen@gmail.com>
2023-05-21 21:00:42 +0500

    fixed task display for each list + remvd dup list

--- index.js
+++ index.js
@@ -1,5 +1,73 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+//code for creating a new todolist on clicking the add new list icon
+const addBox = document.querySelector(".add-box");
+const popupBox = document.querySelector(".popup-box");
+const closeIcon = popupBox.querySelector(".popup span");
+const addNewListButton = popupBox.querySelector(".add");
+openNote = document.querySelectorAll(".note");
+
+todoLists = [];
+localStorage.setItem("todoLists", todoLists);
+let siz = localStorage.getItem("todoLists").length;
+var count = siz;
+
+console.log(siz);
+
+addBox.addEventListener("click", () => {
+  popupBox.classList.add("show");
+  listContainer.innerHTML = "";
+});
+
+closeIcon.addEventListener("click", () => {
+  popupBox.classList.remove("show");
+});
+
+addNewListButton.addEventListener("click", () => {
+  saveTaskData();
+  console.log("clicked");
+});
+
+//making a function to save data
+function saveTaskData() {
+  tasks = [];
+  tasks.push(listContainer.innerHTML);
+  todoLists.push(tasks);
+  console.log(todoLists.length);
+  localStorage.setItem("todoLists", JSON.stringify(todoLists));
+  showLists();
+}
+
+function updateTasksandList(event) {
+  popupBox.classList.add("show");
+  var storedArray = JSON.parse(localStorage.getItem("todoLists"));
+
+  var clickedListHTML = event.currentTarget.innerHTML;
+  var ind = getIndFromHTML(clickedListHTML);
+  console.log(ind);
+  var tasks = storedArray[ind];
+  listContainer.innerHTML = tasks;
+}
+
+function del(e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    console.log("clicked");
+    saveTaskData2(ind);
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveTaskData2(ind);
+  }
+}
+listContainer.addEventListener("click", del, false);
+
+function getIndFromHTML(html) {
+  // Parse the HTML content and extract the value from the span tag
+  var spanTag = document.createElement("span");
+  spanTag.innerHTML = html;
+  var ind = spanTag.querySelector(".brrr").innerHTML;
+  return Number(ind);
+}
 
 function addTask() {
   if (inputBox.value === "") {
@@ -15,7 +83,6 @@ function addTask() {
     li.appendChild(span);
   }
   inputBox.value = "";
-  saveData();
 }
 // Add an event listener to the input field for keyup event
 inputBox.addEventListener("keyup", function (event) {
@@ -29,31 +96,23 @@ inputBox.addEventListener("keyup", function (event) {
 
 //we do this by first adding the event listener to the list-container which is the id name
 //of the ul tag inside which we keep all the tasks
-function del(e) {
-  //this function first checks if the clicked tag is a list tag or a span tah
-  //if its a list one, it toggles the li tag's class name to checked
-  //if its a span then it removes the parent element of the span which is
-  //the list
-  if (e.target.tagName === "LI") {
-    e.target.classList.toggle("checked");
-    saveData();
-  } else if (e.target.tagName === "SPAN") {
-    e.target.parentElement.remove();
-    saveData();
+
+function showLists() {
+  document.querySelectorAll(".note").forEach((note) => note.remove());
+  for (var i = 0; i < todoLists.length; i++) {
+    var liTag = `
+    <li class="note" onClick="updateTasksandList(event)">
+      <p class="details">This is a Title</p>
+      <div class="bottom-content">
+        <span>April 3, 2022</span>
+        <div class="settings">
+          <i class="fas fa-trash-alt outline-icon"></i>
+        </div>
+      </div>
+      <span class="brrr">${i}</span>
+    </li>`;
+    addBox.insertAdjacentHTML("afterend", liTag);
   }
 }
-listContainer.addEventListener("click", del, false);
 
-//making a function to save data
-function saveData() {
-  localStorage.setItem("data", listContainer.innerHTML);
-}
-
-function showTasks() {
-  listContainer.innerHTML = localStorage.getItem("data");
-}
-showTasks();
-
-//code for creating a new todolist on clicking the add new list icon
-const addBox = document.querySelector(".add-box");
-popupBox = document.querySelector(".popup-box");
+showLists();
